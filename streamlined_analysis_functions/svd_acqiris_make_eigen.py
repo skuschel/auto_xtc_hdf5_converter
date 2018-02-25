a645c86ea9ab4c14a6dfbcf658114241fafd0c2a
Sioan <zohar.sioan@gmail.com>
2018-02-25 05:38:22 -0800

    mathematical validation of svd acq

--- streamlined_analysis_functions/svd_acqiris_make_eigen.py
+++ streamlined_analysis_functions/svd_acqiris_make_eigen.py
@@ -28,7 +28,10 @@ def use_acq_svd_basis(detectorObject, thisEvent):
 		
 	#x = detectorObject[selfName](thisEvent)[1][0]
 	for i in arange(len(detectorObject[selfName](thisEvent)[0])):
-		eigen_traces = eigen_traces_h5py["summary/nonMeaningfulCoreNumber0/"+selfName+"/ch"+str(i)+"/eigen_wave_forms"]
+		eigen_traces = eigen_traces_h5py["summary/nonMeaningfulCoreNumber0/"+selfName+"/ch"+str(i)+"/norm_eigen_wave_forms"]
+		#eigen_traces = eigen_traces_h5py["summary/nonMeaningfulCoreNumber0/"+selfName+"/ch"+str(i)+"/eigen_wave_forms"]
+		#eigen_traces = array([eigen_traces[j]/sum(eigen_traces[j]**2)**0.5 for j in arange(len(eigen_traces))])	#not efficient. constantly renormalizing. will optimize later
+		#IPython.embed()
 		y = detectorObject[selfName](thisEvent)[0][i]
 		y -= mean(y[config_parameters['offset_mask']])
 		weightings = dot(eigen_traces,y)
@@ -40,10 +43,10 @@ def use_acq_svd_basis(detectorObject, thisEvent):
 		my_results["ch"+str(i)] = {"weightings":weightings,"variance":variance}
 
 	return my_results
+
 ######################################################
 #######Creating the acqiris eigen basis###############
 ######################################################
-
 def svd_update(eigen_system,new_vector,config_parameters):
 	
 	try:
@@ -56,7 +59,9 @@ def svd_update(eigen_system,new_vector,config_parameters):
 		new_weightings = dot(svd_lsv,diag(singular_values))
 		new_eigen_vectors = dot(pinv(new_weightings),reconstructed_system)[:config_parameters["eigen_basis_size"]]
 
-		eigen_system = {'eigen_weightings':new_weightings,'eigen_wave_forms':new_eigen_vectors}
+		norm_eigen_vectors = real(array([new_eigen_vectors[i]/sum(new_eigen_vectors[i]**2)**0.5 for i in arange(len(new_eigen_vectors))]))
+
+		eigen_system = {'eigen_weightings':new_weightings,'eigen_wave_forms':new_eigen_vectors,'norm_eigen_wave_forms':norm_eigen_vectors}
 	
 	except TypeError:
 		if ((None is new_vector) and (len(eigen_system['eigen_weightings'])>1)):
@@ -64,13 +69,15 @@ def svd_update(eigen_system,new_vector,config_parameters):
 		else:
 			eigen_system['eigen_weightings'] = [1]
 			eigen_system['eigen_wave_forms'] = new_vector
+			eigen_system['norm_eigen_wave_forms'] = new_vector
 
 	except ValueError:
 		
 		if (1==len(eigen_system['eigen_weightings'])):
 			eigen_system['eigen_weightings'] = array([[1,0],[0,1]])
 			eigen_system['eigen_wave_forms'] = vstack([eigen_system['eigen_wave_forms'],new_vector])
-	
+			eigen_system['norm_eigen_wave_forms'] = eigen_system['eigen_wave_forms']		
+
 	return eigen_system
 
 def make_acq_svd_basis(detectorObject,thisEvent,previousProcessing):
@@ -93,9 +100,9 @@ def make_acq_svd_basis(detectorObject,thisEvent,previousProcessing):
 			try:
 				y =  detectorObject[selfName](thisEvent)[0][i]			
 				y -= mean(y[config_parameters['offset_mask']])			
-				eigen_system["ch"+str(i)]= {'eigen_wave_forms':y,'eigen_weightings':[1]}
+				eigen_system["ch"+str(i)]= {'eigen_wave_forms':y,'eigen_weightings':[1],'norm_eigen_wave_forms':[1]}
 			except (KeyError,TypeError) as e:
-				eigen_system["ch"+str(i)] = {'eigen_wave_forms':None,'eigen_weightings':None}
+				eigen_system["ch"+str(i)] = {'eigen_wave_forms':None,'eigen_weightings':None,'norm_eigen_wave_forms':None}
 
 	##############################
 	###main part of calculation###
@@ -109,79 +116,9 @@ def make_acq_svd_basis(detectorObject,thisEvent,previousProcessing):
 
 	return new_eigen_system
 
-
-def make_acq_svd_basis_v1(detectorObject,thisEvent,previousProcessing):
-	
-	thresh_hold = 0.05
-	waveform_window = [1200,1230]
-	eigen_basis_size = 25
-	selfName = detectorObject['self_name']
-	
-	if (0==len(previousProcessing.keys())):
-		eigen_wave_forms = {'ch0':array([]),'ch1':array([]),'ch2':array([]),'ch3':array([])}	#how to make auto sensing size?
-	else:
-		eigen_wave_forms = previousProcessing
-
-	if(None is detectorObject[selfName](thisEvent)):
-		#fit_results = {'amplitude':popt[2],'uncertainty_cov':pcov[2,2]}
-		return eigen_wave_forms
-
-	x = detectorObject[selfName](thisEvent)[1][0]
-
-	#
-
-	for i in arange(len(detectorObject[selfName](thisEvent)[0])):
-
-		y = detectorObject[selfName](thisEvent)[0][i]
-		y -= mean(y[:300])
-		try:
-			#IPython.embed()
-			y /= dot(y,y)
-			y -= dot(dot(eigen_wave_forms['ch'+str(i)],y),eigen_wave_forms['ch'+str(i)])
-			temp = vstack([eigen_wave_forms['ch'+str(i)],y])
-
-		except (ValueError,TypeError,) as e:
-			temp = y/dot(y,y)
-
-			my_max = max(abs(temp))	#generating eigenbasis with 
-			
-			eigen_wave_forms['ch'+str(i)] = y/dot(y,y)**0.5
-
-		try:
-			if(len(temp.shape)<2):
-				raise IndexError
-			
-			#################	
-			#brute force svd approach. too big. fails. use svd approach based on eig below. keeping here so don't make same mistake
-			#u,s,v = svd(temp)	
-			#################
-			#how to preferentially select out peak region without explicit windowing?  Solution directly below
-			#finicky with threshold value
-			#################
-			#thresholded_temp = 0 + temp			
-			#for j in arange(len(temp)):
-			#	my_mask = abs(temp[j])<thresh_hold*max(abs(temp[j]))
-			#	thresholded_temp[j][my_mask] = 0
-			#my_eig = eig(dot(thresholded_temp,thresholded_temp.transpose()))
-			#################
-		
-			#################
-			#brute force windowing
-			my_eig = eig(dot(temp[:,waveform_window[0]:waveform_window[1]],temp[:,waveform_window[0]:waveform_window[1]].transpose()))
-			#################			
-
-			#my_eig = eig(dot(temp,temp.transpose()))
-			v = dot(my_eig[1],temp)
-			#v = array([j/dot(j,j) for j in v])
-
-			eigen_wave_forms['ch'+str(i)] = v[:eigen_basis_size]	
-	
-		except (LinAlgError,IndexError) as e:
-			pass
-
-
-	return eigen_wave_forms
-
+######################################################
+#######End of eigen basis generation##################
+######################################################
 
 def generic_acqiris_analyzer(detectorObject,thisEvent):
 	selfName = detectorObject['self_name']
